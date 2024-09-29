37ea4450d051fff371b121612916a2e6df6eb08b
Оксана <axushka2008@gmail.com>
2024-09-29 18:01:17 +0300

    add task 1-4

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,15 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const massage = `Shipping to ${country} will cost ${totalPrice} credits`;
-    console.log(totalPrice);
-    return massage;
+function formatMessage(message, maxLength) {
+  console.log(message);
+  console.log(message.length );
+console.log(maxLength);
 
+    const usermassage2 = message.slice(0, maxLength) + "...";
+    if(message.length > maxLength) 
+        { return usermassage2;} else if(message.length <= maxLength) { return message;} 
 }
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20)); 
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
